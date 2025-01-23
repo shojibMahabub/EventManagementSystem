4cbc552ca2f490ece7f500f0dc8e57ee49f2bec1
shojib <mahabub@veefin.com>
2025-01-24 00:18:40 +0600

    Task_012 logout route added

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 return [
     '/home' => ['EventController', 'index'],
     '/login' => ['AuthController', 'login'],
+    '/logout' => ['AuthController', 'logout'],
     '/register' => ['AuthController', 'register'],
     '/events' => ['EventController', 'list'],
     '/events/add' => ['EventController', 'add'],
