8cc9d993ee6d5c6d1bef780ebd699978ed25b220
Mahabub Elahi Shojib <mahabub@veefin.com>
2025-01-23 16:02:59 +0600

    Task_012 bootstrap 5 added

--- views/templates/header.php
+++ views/templates/header.php
@@ -3,20 +3,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/styles.css">
     <title>Event Management System</title>
 </head>
 <body>
     <header>
-        <nav>
-            <a href="/">Home</a>
-            <a href="/events">Events</a>
-            <?php if (isset($_SESSION['user_id'])): ?>
-                <a href="/logout">Logout</a>
-            <?php else: ?>
-                <a href="/login">Login</a>
-                <a href="/register">Register</a>
-            <?php endif; ?>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/home">EMS</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/events">Event</a>
+                    </li>
+                    <li class="nav-item">
+                    <?php if (isset($_SESSION['user_id'])): ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/logout">Logout</a>
+                        </li>
+                    <?php else: ?>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/login">Login</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link active" href="/register">Register</a>
+                        </li>
+                    <?php endif; ?>
+                    </li>
+                </ul>
+                </div>
+            </div>
         </nav>
+
+
+
+
+
     </header>
-    <main>
